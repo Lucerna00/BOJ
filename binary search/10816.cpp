b23d22640381eaad6992f3859639fc0a96e43d45
Junseok Park <pjs3859811@naver.com>
2022-07-10 19:32:04 +0900

    숫자 카드 2

---
+++ binary search/10816.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N = 0, num = 0, M = 0;
+	vector<int> card;
+
+	cin >> N;
+	while (N--) {
+		cin >> num;
+		card.push_back(num);
+	}
+
+	sort(card.begin(), card.end());
+
+	cin >> M;
+	while (M--) {
+		cin >> num;
+		cout << upper_bound(card.begin(), card.end(), num) - lower_bound(card.begin(), card.end(), num) << ' ';
+	}
+	return 0;
+}
