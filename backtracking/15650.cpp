3f973e5241a4e208048456ffd97c7a4c3259b88f
Junseok Park <pjs3859811@naver.com>
2022-08-20 21:32:55 +0900

    N과 M (2)

---
+++ backtracking/15650.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int N = 0, M = 0, output[8];
+
+void dfs(int, int);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	dfs(0, 1);
+	return 0;
+}
+
+void dfs(int K, int start) {
+	if (K == M) {
+		for (int i = 0;i < M;i++)
+			cout << output[i] << ' ';
+		cout << '\n';
+	}
+	else {
+		for (int i = start;i <= N;i++) {
+			output[K] = i;
+			dfs(K + 1, i + 1);
+		}
+	}
+}	
