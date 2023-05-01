495f80f438c02176fc2da9c96b5202f65fc890b7
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2023-05-01 23:18:35 +0900

    돌 게임 4

---
+++ dynamic programming 1/9658.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int dp[1000] = { 0,1,0,1, };
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int	N = 0;
+
+	cin >> N;
+
+	for (int i = 4;i < N;i++) {
+		if (min({ dp[i - 1], dp[i - 3], dp[i - 4] }) == 1)
+			dp[i] = 0;
+		else
+			dp[i] = 1;
+	}
+
+	if (dp[N - 1] == 1)
+		cout << "SK";
+	else
+		cout << "CY";
+	return 0;
+}
