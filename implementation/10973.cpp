651b5a6f109aec87edde24afeaddfe14b493bca0
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2023-05-07 23:42:45 +0900

    이전 순열

---
+++ implementation/10973.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, arr[10000] = { 0, };
+
+	cin >> N;
+	for (int i = 0;i < N;i++)
+		cin >> arr[i];
+
+	if (prev_permutation(arr, arr + N)) {
+		for (int i = 0;i < N;i++)
+			cout << arr[i] << " ";
+	}
+	else
+		cout << -1;
+	return 0;
+}
