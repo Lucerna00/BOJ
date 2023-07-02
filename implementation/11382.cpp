08376141aa25064e1e7d2d6900093dd7c9a67d3f
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2023-07-02 23:56:24 +0900

    꼬마 정민

---
+++ implementation/11382.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long A = 0, B = 0, C = 0;
+
+	cin >> A >> B >> C;
+
+	cout << A + B + C;
+	return 0;
+}
