55afea7185285aa7109ee70df39665fff7d83547
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2024-01-01 23:41:51 +0900

    골뱅이 찍기 - 뒤집힌 ㄱ

---
+++ implementation/23802.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N = 0;
+
+	cin >> N;
+
+	for (int i = 0;i < 5 * N;i++) {
+		for (int j = 0;j < 5 * N;j++) {
+			if (!(i >= N && j >= N))
+				cout << '@';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
