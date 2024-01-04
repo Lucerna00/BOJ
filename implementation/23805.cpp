4117611435ed1efb152227190ad37e3cb1d5e1c8
Junseok Park <95912522+Lucerna00@users.noreply.github.com>
2024-01-04 21:39:15 +0900

    골뱅이 찍기 - 돌아간 ㄹ

---
+++ implementation/23805.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N = 0;
+
+	cin >> N;
+
+	for (int i = 0;i < 5 * N;i++) {
+		for (int j = 0;j < 5 * N;j++) {
+			if ((i >= N && (j >= N && j < 2 * N)) || (i < 4 * N && (j >= 3 * N && j < 4 * N)))
+				cout << ' ';
+			else
+				cout << '@';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
